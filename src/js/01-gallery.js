53ec3de39f55726f4f618dd6a557e9338917f844
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-21 15:37:47 +0100

    1-gallery rdy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(simpleLightbox);
+
+const imageGallery = document.querySelector(".gallery")
+
+const markup = galleryItems.map(item =>
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("")
+
+imageGallery.insertAdjacentHTML('beforeend', markup)
+imageGallery.addEventListener("click", onClick)
+
+console.log(SimpleLightbox)
+
+function onClick(event) {
+    event.preventDefault();
+    var lightbox = new SimpleLightbox('.gallery a', {scrollZoom:false, captionsData:"alt", captionDelay:250});
+}
