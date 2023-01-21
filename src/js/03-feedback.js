c4a33c4ac43aa3c99022fd48059e43b84a60d1d5
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-21 20:21:52 +0100

    3. form rdy

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form")
+// console.log(form)
+
+form.addEventListener("input", throttle(onInput,500))
+form.addEventListener("submit", onSubmit)
+const LOCALSTORAGE_KEY = "feedback-form-state";
+// const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+let formData = {}
+
+onUpdate();
+
+function onInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+    evt.preventDefault()
+    const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(savedData)
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+    form.reset()
+}
+
+function onUpdate() {
+    if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+        return
+    }
+    const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(savedData)
+    if (savedData.email) { 
+    form.elements.email.value = savedData.email;
+    }
+    if (savedData.message) {
+    form.elements.message.value = savedData.message;
+    }
+};
